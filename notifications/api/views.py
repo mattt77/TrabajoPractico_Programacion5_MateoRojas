62ad77747eeffd3d810e76b355ec565a904f8557
mattt77 <mateorojas.s.c@gmail.com>
2026-06-17 20:40:19 -0300

    feat: separa notifications en estructura api/

--- notifications/api/views.py
+++ notifications/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from .models import Notification
+from ..models import Notification
 from .serializers import NotificationSerializer
 
 
