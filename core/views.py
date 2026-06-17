4eb6bd08d8d2b2fb84e1d93620c491f88bbb8a5c
mattt77 <mateorojas.s.c@gmail.com>
2026-06-17 20:57:35 -0300

    chore: actualiza gitignore y limpia core/views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def api_root(request, format=None):
+    return Response({
+        'auth': {
+            'login (obtener token)': reverse('token_obtain_pair', request=request, format=format),
+            'refresh token': reverse('token_refresh', request=request, format=format),
+            'register (solo admin)': reverse('register', request=request, format=format),
+        },
+        'users': {
+            'lista de usuarios': reverse('user-list', request=request, format=format),
+            'usuario actual': reverse('user-me', request=request, format=format),
+        },
+        'boards': reverse('board-list', request=request, format=format),
+        'lists': reverse('list-list', request=request, format=format),
+        'cards': reverse('card-list', request=request, format=format),
+        'notifications': reverse('notification-list', request=request, format=format),
+    })
